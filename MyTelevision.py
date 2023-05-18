1b11ba0f998395497b3e2f1518b6a0b9b2f1f43e
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-19 00:55:06 +0800

    tv class to def get/set channels

--- MyTelevision.py
+++ MyTelevision.py
@@ -1,10 +1,26 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.6
 
 # Make the television class
-# Tv is initially turned off, default channel and volume
-# Def turn on and off
-# Def get and set channels
-# Def get and set volume
+class MYTELEVISION:
+    # Tv is initially turned off, default channel and volume
+    def __init__(self):
+        self.channel = 1
+        self.volume = 1
+        self.on = False
+    
+    # Def turn on and off
+    def turn_on(self):
+        self.on = True
+    def turn_off(self):
+        self.on = False
+
+    # Def get and set channels
+    def get_channels(self):
+        self.channel = self.channel
+    def set_channels(self, channels):
+        self.channel = channels
+
+    # Def get and set volume
 # Def up and down channels
 # Def up and down volume
 
