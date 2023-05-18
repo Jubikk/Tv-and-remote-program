7643c1d2d847d86892c58cbb15a85e35ab1f3983
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-19 02:20:22 +0800

    added the remote class

--- Remote.py
+++ Remote.py
@@ -1,10 +1,26 @@
 #Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.6 (Remote)
 
-# import tkinter as tk
-# from MyTelevision import MYTELEVISION
+# import tkinter and the file
+import tkinter as tk
+from MyTelevision import MYTELEVISION
 
 
 # Make the remote class
+class MyRemote:
+    def __init__(self , main):
+        self.main = main
+        self.tv1 = MYTELEVISION()
+        self.tv2 = MYTELEVISION()
+
+        self.create_remote()
+    
+    def create_remote(self):
+        self.tv1_frame = tk.Frame(self.main)
+        self.tv1_frame.pack()
+
+
+
+
 # Def init
 # Def widget(create widget)
 # Def channel up and down
