d179031595ba95b0243730f5c994b5b527de2079
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-19 00:47:43 +0800

    Pseudocode

---
+++ MyTelevision.py
@@ -0,0 +1,11 @@
+#Joebeck Andrew F. Gusi | BSCPE 1-5 | Assignment No.6
+
+# Make the television class
+# Tv is initially turned off, default channel and volume
+# Def turn on and off
+# Def get and set channels
+# Def get and set volume
+# Def up and down channels
+# Def up and down volume
+
+#end
