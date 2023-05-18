8bc2a783773ac076fd60f34ae5772d58e0c8345a
Joebeck Andrew F. Gusi <joebeckgusi25@gmail.com>
2023-05-19 02:50:15 +0800

    the code was already completed, it runs but it says attribute error

--- Remote.py
+++ Remote.py
@@ -20,13 +20,13 @@ class MyRemote:
         self.TV1_frame = tk.Frame(self.main)
         self.TV1_frame.pack(pady=10)
 
-        self.TV1_label = tk.Label(self.tv1_frame, text="The channel of TV1 is 1 and the volume level is 1.")
+        self.TV1_label = tk.Label(self.TV1_frame, text="The channel of TV1 is 1 and the volume level is 1.")
         self.TV1_label.pack(padx=10, pady=15)
 
         self.TV2_frame = tk.Frame(self.main)
         self.TV2_frame.pack(pady=10)
 
-        self.TV2_label = tk.Label(self.tv2_frame, text="The channel of TV2 is 1 and the volume level is 1.")
+        self.TV2_label = tk.Label(self.TV2_frame, text="The channel of TV2 is 1 and the volume level is 1.")
         self.TV2_label.pack(padx=10, pady=15)
 
         self.channel_up_button = tk.Button(self.main, text="Channel Up", command=self.channel_up)
@@ -40,3 +40,43 @@ class MyRemote:
 
         self.volume_down_button = tk.Button(self.main, text="Volume Down", command=self.volume_down)
         self.volume_down_button.pack(pady=10)
+    
+    def channel_up(self):
+        self.TV1.channel_up()
+        self.TV2.channel_up()
+        self.update_label()
+
+    def channel_down(self):
+        self.TV1.channel_down()
+        self.TV2.channel_down()
+        self.update_label()
+
+    def volume_up(self):
+        self.TV1.volume_up()
+        self.TV2.volume_up()
+        self.update_label()
+
+    def volume_down(self):
+        self.TV1.volume_down()
+        self.TV2.volume_down()
+        self.update_label()
+    
+    def update_labels(self):
+        TV1_channel = self.TV1.get_channel()
+        TV1_volume = self.TV1.get_volume()
+        self.TV1_label.config(text="TV1's channel is () and volume level is ()".format(TV1_channel, TV1_volume))
+
+        TV2_channel = self.TV2.get_channel()
+        TV2_volume = self.TV2.get_volume()
+        self.TV2_label.config(text="TV2's channel is () and volume level is ()".format(TV2_channel, TV2_volume))
+
+def main():
+    root = tk.Tk()
+    root.geometry("400x350")  
+    root.title("Remote Control Panel")  
+    app = MyRemote(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
+    
